b30e0a25cb813d18be971d22c98891ec88090790
sagarsinghrajawat12 <warshasingh21@gmail.com>
2024-10-19 17:50:45 +0530

    starting new project

--- src/App.js
+++ src/App.js
@@ -3,22 +3,7 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <header>
-        <div className="left">
-          <nav>
-            <ul className="leftone">
-              <li>Home</li>
-              <li>About</li>
-              <li>Docs</li>
-              <li>Contact Me</li>
-              <li>Source CODE</li>
-            </ul>
-          </nav>
-        </div>
-        <div className="right"></div>
-      </header>
-    </>
+    
   );
 }
 
