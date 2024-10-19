a50f2bfd619a4d492317320cf598e048212a7d33
sagarsinghrajawat12 <warshasingh21@gmail.com>
2024-10-19 17:27:15 +0530

    making nav bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <div className="left">
+          <nav>
+            <ul className="leftone">
+              <li>Home</li>
+              <li>About</li>
+              <li>Docs</li>
+              <li>Contact Me</li>
+              <li>Source CODE</li>
+            </ul>
+          </nav>
+        </div>
+        <div className="right"></div>
       </header>
-    </div>
+    </>
   );
 }
 
