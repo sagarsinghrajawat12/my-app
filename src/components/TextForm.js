1cebed7a9934aed6b7e0d7e99fea4e21b8d27cc0
sagarsinghrajawat12 <warshasingh21@gmail.com>
2024-10-20 14:30:10 +0530

    add new button of lower case

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 
 export default function TextForm(props) {
+    const handeldwclick = ()=>{
+        let newText = text.toLocaleLowerCase();
+        setText(newText)
+    }
     const handelUPclick = () => {
         let newText = text.toUpperCase();
         setText(newText)
@@ -12,12 +16,22 @@ export default function TextForm(props) {
     const [text, setText] = useState('write your text here');
 
     return (
-        <div>
+        <>
+        <div className='container'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleonchange} id="exampleFormControlTextarea1" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handelUPclick}>CONVERT TO UPPERCASE</button>
+            <button className="btn btn-primary mx-3 " onClick={handelUPclick}>CONVERT TO UPPERCASE</button>
+            <button className="btn btn-primary mx-3" onClick={handeldwclick}>Convert to lowercase</button>
         </div>
+        <div className="container my-4">
+            <h1>Your text summary </h1>
+            <p>{text.split(" ").length} words  and {text.length} characters</p>
+            <p>{0.008*text.split(" ").length} Meanute in read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
